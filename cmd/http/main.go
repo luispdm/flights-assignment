3a9e6a0a39d102fae40c9c6fb8639d831ed6434e
luispdm <17044119+luispdm@users.noreply.github.com>
2022-07-27 13:07:31 +0200

    add timeouts

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -16,8 +17,12 @@ func run() error {
 	port := "8080"
 	log.Printf("log level 'info' - main: starting application on port '%s'...", port)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: routes(),
+		Addr:              ":" + port,
+		Handler:           routes(),
+		ReadTimeout:       10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		ReadHeaderTimeout: 10 * time.Second,
+		IdleTimeout:       10 * time.Second,
 	}
 	err := srv.ListenAndServe()
 	if err != nil {
